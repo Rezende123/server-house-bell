08f026d9fb3cc258f61d30520f3d384c7a288e35
Felipe Rezende <feliperezende.srz@gmail.com>
2020-03-22 14:15:47 -0300

    Habilitando o onesingal para notificar sobre a campainha

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const OneSignal = require('onesignal-node');
+const client = new OneSignal.Client(
+    '1096356845228', 
+    'AAAA_0P11qw:YjRmOTgwODMtNGE1ZC00ZjhiLTk0ZmUtNWM3NmYxN2Q4ZDNl'
+);
 const express = require('express')
     , bodyParser = require('body-parser')
     , cors = require('cors')
@@ -8,6 +13,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
+const notification = {
+    contents: {
+      'pt': 'Tem alguém tocando a campainha.',
+      'en': 'Have someone ringing the bell.',
+    }
+  };
 /* Routes */
 app.post('/ring-the-bell', (req, res) => {
     let response = {sendNotification: false};
@@ -15,9 +26,19 @@ app.post('/ring-the-bell', (req, res) => {
     if (req.body.ringingTheBell) {
         console.log('TOCANDO A CAMPAINHA');
         response.sendNotification = true;
+        client.createNotification(notification)
+            .then(res => {
+                console.log('CAMPAINHA TOCADA');
+                res.json(res);
+            })
+            .catch(err => {
+                console.log('ERRO AO NOTIFICAR');
+                res.json(err);
+            })
+    } else {
+        res.json({sendNotification: false});
     }
 
-    res.json(response);
 });
 
 /* App listen */
