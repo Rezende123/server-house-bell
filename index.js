3dcac74bb257b1a571e6221a4233be51f31bcc54
Felipe Rezende <feliperezende.srz@gmail.com>
2020-03-23 08:18:16 -0300

    Corrigindo as chaves e o conteúdo do onesignal

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const OneSignal = require('onesignal-node');
 const client = new OneSignal.Client(
-    '1096356845228', 
-    'AAAA_0P11qw:YjRmOTgwODMtNGE1ZC00ZjhiLTk0ZmUtNWM3NmYxN2Q4ZDNl'
+    'dd72b879-4174-4fef-8ba5-1faa0ff6cb94', 
+    'YjRmOTgwODMtNGE1ZC00ZjhiLTk0ZmUtNWM3NmYxN2Q4ZDNl'
 );
 const express = require('express')
     , bodyParser = require('body-parser')
@@ -14,26 +14,30 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 const notification = {
+    android_channel_id: '47549fb7-382e-462c-98b7-a4b59e21ea37',
+    headings: {
+        'pt': 'Atenção à porta',
+        'en': 'Warning to door',
+    },
     contents: {
-      'pt': 'Tem alguém tocando a campainha.',
-      'en': 'Have someone ringing the bell.',
-    }
-  };
+        'pt': 'Tem alguém tocando a campainha.',
+        'en': 'Have someone ringing the bell.',
+    },
+    included_segments: ['Subscribed Users']
+};
 /* Routes */
 app.post('/ring-the-bell', (req, res) => {
     let response = {sendNotification: false};
 
     if (req.body.ringingTheBell) {
-        console.log('TOCANDO A CAMPAINHA');
         response.sendNotification = true;
         client.createNotification(notification)
-            .then(res => {
-                console.log('CAMPAINHA TOCADA');
-                res.json(res);
+            .then(resNotification => {
+                res.json({resNotification});
             })
             .catch(err => {
-                console.log('ERRO AO NOTIFICAR');
-                res.json(err);
+                console.log('ERRO AO NOTIFICAR: \n', err);
+                res.json({err});
             })
     } else {
         res.json({sendNotification: false});
